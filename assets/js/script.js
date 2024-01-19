7239375f287807a774506740f6dba5616c1b99d0
Francesca Franz <francesca.franz.33@gmail.com>
2024-01-19 18:43:34 +0100

    Header, Home e About in CSS. Correzioni

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,16 @@
 //scalabilità
-if (document.documentElement.clientWidth < 480) { 
+if (document.documentElement.clientWidth < 768) { 
   document.querySelector("meta[name=viewport]").setAttribute(
         'content', 
         'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0');
 }
- 
-//nomi delle schede (tabname)
-  var tablinks = document.getElementsByClassName("tab-links");
-  var tabcontents = document.getElementsByClassName("tab-contents");
-
-  function opentab(tabname){  
-      for(tablink of tablinks){ 
-          tablink.classList.remove("active-link");
-      }
-      for(tabcontent of tabcontents){ 
-          tabcontent.classList.remove("active-tab");
-      }
-      event.currentTarget.classList.add("active-link");
-      document.getElementById(tabname).classList.add("active-tab");
-  }
 
 //menù laterale
-  var sidemenu = document.getElementById("sidemenu");
 
   function openmenu(){
-      sidemenu.style.right = "0";
+    document.getElementById("sidemenu").style.right= "0";
   }
 
   function closemenu(){
-      sidemenu.style.right = "-200px";
+    document.getElementById("sidemenu").style.right = "-240px";
   }
